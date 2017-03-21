e05e6af272043a726e04fefd58ed8b0ef492e0aa
Rachel <rachel.loziuk@gmail.com>
2017-03-20 22:05:10 -0500

    user can save a new 'people' instance to the database

--- app/controllers/PeopleController.php
+++ app/controllers/PeopleController.php
@@ -2,124 +2,126 @@
 
 class PeopleController extends \BaseController {
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		$people = People::all();
-	}
+  /**
+   * Display a listing of the resource.
+   *
+   * @return Response
+   */
+  public function index()
+  {
+    $people = People::all();
+
+  }
 
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return Response
+   */
+  public function create()
+  {
+    //
+  }
 
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//VALIDATE USER INPUT IS JSON
-		$people_json = json_decode(Input::get('people'));
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @return Response
+   */
+  public function store()
+  {
+    $json = json_decode(Input::get('people'));
 
-		if( !$people_json )
-		{
-			return Redirect::to('/')->with('message', "Oops! It appears you didn't enter proper json format. Here's a quick reference guide: <a href='http://secretgeek.net/json_3mins' target='_blank'>3-Minute Guide to Json</a>")->with('alert-class', "alert-warning");
-		}
+    // DISPLAY ERROR MESSAGE IF USER DID NOT ENTER CORRECT JSON FORMAT":
+    if( !$json )
+    {
+      return Redirect::to('/')->with('message', "Oops! It appears you didn't enter proper json format. Here's a quick reference guide: <a href='http://secretgeek.net/json_3mins' target='_blank'>3-Minute Guide to Json</a>")->with('alert-class', "alert-warning");
+    }
 
-    
-    // this should be a fucntion in the People model
-    $persons = $people_json->data;
+    // GET EMAILS &
+    // CREATE FULL NAME VALUE FOR PEOPLE:
+    $people_data = $json->data;
     $emails = [];
 
-    foreach ($persons as $person) {
+    foreach ($people_data as $person) {
       $emails[] = $person->email;
+      $person->name = $person->first_name . ' ' . $person->last_name; // INSERT A 'NAME' VALUE
     }
 
     $email_list = implode(',', $emails);
-    // end function
 
-    // this should be a fucntion in the People model
-
-    // Sort the array using a user defined function
-    usort($persons, function($a, $b) { 
+    //SORT PEOPLE DESCENDING BY AGE:
+    usort($people_data, function($a, $b) { 
       return $a->age > $b->age ? -1 : 1; //Compare ages
     });
 
-    print_r($persons);   
+    $age_sorted = new stdClass();
+    $age_sorted->data = $people_data;
+
+    $json_age_sorted = json_encode($age_sorted);
+
+    // SAVE REFORMATTED PEOPLE INFO:
+    $people = new People;
+    $people->emails = $email_list;
+    $people->age_sorted = $json_age_sorted;
+
+    if ( $people->save() )
+    {
+      return Redirect::to('/')->with('message', "You have successfully added some people to the database!")->with('alert-class', "alert-success");
+
+    }
 
 
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return Response
+   */ 
+  public function show($id)
+  {
     //
+  }
 
 
-	}
-
-	// {
-	// 	"data":[
-	// 		{ "first_name":"matt", "last_name":"stauffer", "age":31, "email":"matt@stauffer.com", "secret":"VXNlIHRoaXMgc2VjcmV0IHBocmFzZSBzb21ld2hlcmUgaW4geW91ciBjb2RlJ3MgY29tbWVudHM="},
-	// 		{"first_name":"dan", "last_name":"sheetz", "age":99, "email": "dan@sheetz.com", "secret":"YWxidXF1ZXJxdWUuIHNub3JrZWwu" }
-	// 		]
-	// 	}
-
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */ 
-	public function show($id)
-	{
-		//
-	}
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function edit($id)
+  {
+    //
+  }
 
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function update($id)
+  {
+    //
+  }
 
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function destroy($id)
+  {
+    //
+  }
 
 
 }
